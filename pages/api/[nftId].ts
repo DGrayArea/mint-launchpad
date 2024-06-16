a7d45f253a12b5fed7c0019669b18893cfc46890
AlexCPT-cpu <alexodey79@gmail.com>
2024-06-16 15:10:01 +0100

    change

--- pages/api/[nftId].ts
+++ pages/api/[nftId].ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function handler(req: any, res: any) {
   const { nftId } = req.query;
   const { data } = await axios.get(
-    `https://ipfs.io/ipfs/bafybeiaefvjpb4d2pzgbsipomb52gmbtzk6ybxjr3fko34avdiusq457vq/${nftId}.json`
+    `https://bafybeiaefvjpb4d2pzgbsipomb52gmbtzk6ybxjr3fko34avdiusq457vq.ipfs.nftstorage.link/${nftId}.json`
   );
 
   res.status(200).json(data);
