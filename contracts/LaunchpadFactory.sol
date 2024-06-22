4b6d2c809494032bf249a9f3306f6b0358f62e9b
AlexCPT-cpu <alexodey79@gmail.com>
2024-06-23 00:54:06 +0100

    update nft base

--- contracts/LaunchpadFactory.sol
+++ contracts/LaunchpadFactory.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./HorrorApeClub.sol";
+import "./NftBase.sol";
 
 contract LaunchpadFactory {
-    HorrorApeClub[] public collections;
+    NftBase[] public collections;
 
     function createCollection(
         string memory _name,
@@ -19,35 +19,6 @@ contract LaunchpadFactory {
     ) public {
         MintPhase memory whitelistPhase = MintPhase(_whitelistMint, 0, _whitelistLimit, 0);
         MintPhase memory fcfsPhase = MintPhase(false, 0, 0, 0);
-        MintPhase memory publicPhase = MintPhase(_publicMint, 0.01 ether, _publicLimit, 0);
-        
-        HorrorApeClub newCollection = new HorrorApeClub(
-            _name,
-            _symbol,
-            _initBaseURI,
-            _initNotRevealedUri,
-            whitelistPhase,
-            fcfsPhase,
-            publicPhase,
-            _creator
-        );
-        collections.push(newCollection);
-    }
-
-    function createCollectionWithFCFS(
-        string memory _name,
-        string memory _symbol,
-        string memory _initBaseURI,
-        string memory _initNotRevealedUri,
-        bool _publicMint,
-        bool _whitelistMint,
-        uint256 _whitelistLimit,
-        uint256 _fcfsLimit,
-        uint256 _publicLimit,
-        address _creator
-    ) public {
-        MintPhase memory whitelistPhase = MintPhase(_whitelistMint, 0, _whitelistLimit, 0);
-        MintPhase memory fcfsPhase = MintPhase(true, 0, _fcfsLimit, 0);
         MintPhase memory publicPhase = MintPhase(_publicMint, 0.01 ether, _publicLimit, 0);
         
         HorrorApeClub newCollection = new HorrorApeClub(
--- contracts/NftBase.sol
+++ contracts/NftBase.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract HorrorApeClub is ERC721Enumerable, Ownable, ReentrancyGuard {
+contract NftBase is ERC721Enumerable, Ownable, ReentrancyGuard {
     using Strings for uint256;
 
     struct MintPhase {
