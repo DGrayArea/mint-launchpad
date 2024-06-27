85ee4a528738c087a9a5d6abfa7f81451734907f
AlexCPT-cpu <alexodey79@gmail.com>
2024-06-27 18:06:17 +0100

    done

--- components/Collection/Tabs/Fcfs.tsx
+++ components/Collection/Tabs/Fcfs.tsx
@@ -137,9 +137,9 @@ const FCFS = () => {
       {mintDone || mintAmount === 1 ? (
         <button
           disabled
-          className="w-full mt-1.5 relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium px-4 py-3 sm:px-6 cursor-not-allowed disabled:bg-opacity-70 bg-yellow-400 hover:bg-yellow-700 text-neutral-50 flex-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
+          className="w-full mt-1.5 relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium px-4 py-3 sm:px-6 cursor-not-allowed disabled:bg-opacity-70 bg-red-400 hover:bg-red-700 text-neutral-50 flex-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
         >
-          FCFS Mint Pending
+          FCFS Mint Ended
         </button>
       ) : (
         <button
--- components/Collection/Tabs/Public.tsx
+++ components/Collection/Tabs/Public.tsx
@@ -153,9 +153,9 @@ const Public = () => {
       {mintDone || mintAmount >= 5 ? (
         <button
           disabled
-          className="w-full mt-1.5 relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium px-4 py-3 sm:px-6 cursor-not-allowed disabled:bg-opacity-70 bg-yellow-400 hover:bg-yellow-700 text-neutral-50 flex-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
+          className="w-full mt-1.5 relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium px-4 py-3 sm:px-6 cursor-not-allowed disabled:bg-opacity-70 bg-red-400 hover:bg-red-700 text-neutral-50 flex-1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
         >
-          Mint Pending
+          Mint Ended
         </button>
       ) : (
         <button
